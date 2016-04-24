5917eef28f8d6eb52a29b275bf286e5b3907be5a
Bánszki István <banszki.istvan@outlook.com>
2016-04-24 23:21:29 +0200

    Initial files for the web module.

---
+++ worktime-web/src/main/webapp/js/app.js
@@ -0,0 +1,17 @@
+var app = angular.module('exampleApp', ['ngRoute'])
+
+module.config(['$routeProvider',
+    function($routeProvider) {
+        $routeProvider.
+            when('/route1/:param', {
+                templateUrl: 'angular-route-template-1.jsp',
+                controller: 'RouteController'
+            }).
+            when('/route2/:param', {
+                templateUrl: 'angular-route-template-2.jsp',
+                controller: 'RouteController'
+            }).
+            otherwise({
+                redirectTo: '/'
+    		});
+}]);
