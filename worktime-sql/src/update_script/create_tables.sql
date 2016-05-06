9abe5f39569e8d0eecbb5063e3549b0e986b135a
Bánszki István <Bnszki Istvn>
2016-05-06 19:56:26 +0200

    Some minor changes in SQL files.

--- worktime-sql/src/update_script/create_tables.sql
+++ worktime-sql/src/update_script/create_tables.sql
@@ -1,64 +1,14 @@
-/* ***************************************************
-			worker_holiday_number table creation
-   *************************************************** */
-
-DROP TABLE IF EXISTS `worker_holiday_number`;
-CREATE TABLE `worker_holiday_number` (
-  `id` bigint(20) NOT NULL,
-  `year` int(4) NOT NULL,
-  `worker_holiday_number_total` int(2) NOT NULL,
-  `date_of_registration` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `worker_id` bigint(20) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FK_whn_worker_id_idx` (`worker_id`),
-  CONSTRAINT `FK_whn_worker_id` FOREIGN KEY (`worker_id`) REFERENCES `worker` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+USE worktime;
 
 /* ***************************************************
-				worker table creation
+					role table creation
    *************************************************** */
-
-DROP TABLE IF EXISTS `worker`;
-CREATE TABLE `worker` (
+   
+DROP TABLE IF EXISTS `role`;
+CREATE TABLE `role` (
   `id` bigint(20) NOT NULL,
-  `first_name` tinytext NOT NULL,
-  `last_name` tinytext NOT NULL,
-  `gender` int(1) NOT NULL,
-  `date_of_birth` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `nationality` tinytext NOT NULL,
-  `position` tinytext NOT NULL,
-  `email_address` tinytext NOT NULL,
-  `daily_work_hour_total` tinyint(4) NOT NULL,
-  `superior_work_id` bigint(20) NOT NULL,
-  `department_id` bigint(20) NOT NULL,
-  `user_id` bigint(20) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FK_w_user_id_idx` (`user_id`),
-  KEY `FK_w_department_id_idx` (`department_id`),
-  CONSTRAINT `FK_w_department_id` FOREIGN KEY (`department_id`) REFERENCES `department` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `FK_w_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-/* ***************************************************
-				work_log table creation
-   *************************************************** */
-
-DROP TABLE IF EXISTS `work_log`;
-CREATE TABLE `work_log` (
-  `id` bigint(20) NOT NULL,
-  `description` tinytext NOT NULL,
-  `status` int(1) NOT NULL,
-  `begin` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `end` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `date_of_modification` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `date_of_registration` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `absence_type_id` bigint(20) NOT NULL,
-  `worker_id` bigint(20) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FK_wl_worker_id_idx` (`worker_id`),
-  KEY `FK_wl_absence_type_id_idx` (`absence_type_id`),
-  CONSTRAINT `FK_wl_absence_type_id` FOREIGN KEY (`absence_type_id`) REFERENCES `absence_type_id` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `FK_wl_worker_id` FOREIGN KEY (`worker_id`) REFERENCES `worker` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  `role` tinytext NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 /* ***************************************************
@@ -75,17 +25,6 @@ CREATE TABLE `user` (
   PRIMARY KEY (`id`),
   KEY `FK_u_role_id_idx` (`role_id`),
   CONSTRAINT `FK_u_role_id` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-/* ***************************************************
-					role table creation
-   *************************************************** */
-   
-DROP TABLE IF EXISTS `role`;
-CREATE TABLE `role` (
-  `id` bigint(20) NOT NULL,
-  `role` tinytext NOT NULL,
-  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 /* ***************************************************
@@ -127,4 +66,67 @@ CREATE TABLE `absence_type_id` (
   `id` bigint(20) NOT NULL,
   `name` tinytext NOT NULL,
   PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/* ***************************************************
+				worker table creation
+   *************************************************** */
+
+DROP TABLE IF EXISTS `worker`;
+CREATE TABLE `worker` (
+  `id` bigint(20) NOT NULL,
+  `first_name` tinytext NOT NULL,
+  `last_name` tinytext NOT NULL,
+  `gender` int(1) NOT NULL,
+  `date_of_birth` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `nationality` tinytext NOT NULL,
+  `position` tinytext NOT NULL,
+  `email_address` tinytext NOT NULL,
+  `daily_work_hour_total` tinyint(4) NOT NULL,
+  `superior_work_id` bigint(20) NOT NULL,
+  `department_id` bigint(20) NOT NULL,
+  `user_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_w_user_id_idx` (`user_id`),
+  KEY `FK_w_department_id_idx` (`department_id`),
+  CONSTRAINT `FK_w_department_id` FOREIGN KEY (`department_id`) REFERENCES `department` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `FK_w_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/* ***************************************************
+			worker_holiday_number table creation
+   *************************************************** */
+
+DROP TABLE IF EXISTS `worker_holiday_number`;
+CREATE TABLE `worker_holiday_number` (
+  `id` bigint(20) NOT NULL,
+  `year` int(4) NOT NULL,
+  `worker_holiday_number_total` int(2) NOT NULL,
+  `date_of_registration` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `worker_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_whn_worker_id_idx` (`worker_id`),
+  CONSTRAINT `FK_whn_worker_id` FOREIGN KEY (`worker_id`) REFERENCES `worker` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/* ***************************************************
+				work_log table creation
+   *************************************************** */
+
+DROP TABLE IF EXISTS `work_log`;
+CREATE TABLE `work_log` (
+  `id` bigint(20) NOT NULL,
+  `description` tinytext NOT NULL,
+  `status` int(1) NOT NULL,
+  `begin` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `end` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `date_of_modification` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `date_of_registration` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `absence_type_id` bigint(20) NOT NULL,
+  `worker_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_wl_worker_id_idx` (`worker_id`),
+  KEY `FK_wl_absence_type_id_idx` (`absence_type_id`),
+  CONSTRAINT `FK_wl_absence_type_id` FOREIGN KEY (`absence_type_id`) REFERENCES `absence_type_id` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `FK_wl_worker_id` FOREIGN KEY (`worker_id`) REFERENCES `worker` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
