54131655734ebf11905afe047bb4bdb3b1df76aa
IstvanBanszki <banszki.istvan@outlook.com>
2016-09-24 17:01:23 +0200

    Addition page - Angular JS refactor based on John Papa style guide

--- worktime-core/src/main/webapp/modules/addition/addition.controller.js
+++ worktime-core/src/main/webapp/modules/addition/addition.controller.js
@@ -1,173 +1,222 @@
-'use strict';
+(function() {
+	'use strict';
 
-angular.module('Addition')
-.controller('AdditionController', ['$scope', '$rootScope', 'AdditionService',
-    function($scope, $rootScope, AdditionService) {
-		$scope.officesWithDepartments = [];
-		$scope.departments = [];
-		$scope.offices = [];
-		$scope.superiors = [];
-		$scope.departmentsForOffice = [];
+	angular
+		.module('Addition')
+		.controller('AdditionController', AdditionController);
 
-		$scope.indexOfSelectedOffice = -1;
-		$scope.indexOfSelectedDepartment = -1;
+	AdditionController.$inject = ['$rootScope', 'AdditionService']
 
-		$scope.selectedDepartmentName = "";
-		$scope.selectedDepartment = "";
-		$scope.selectedOfficeName = "";
-		$scope.selectedOffice = "";
+    function AdditionController($rootScope, AdditionService) {
 
-		$scope.showODInformation = false;
-		$scope.showEditOfficeForm = false;
-		$scope.showEditDepartmentForm = false;
-		$scope.showSaveOfficeForm = false;
-		$scope.showSaveDepartmentForm = false;
+		var vm = this;
+		//Bindable variables
+		vm.officesWithDepartments = [];
+		vm.departments = [];
+		vm.offices = [];
+		vm.superiors = [];
+		vm.departmentsForOffice = [];
+		vm.indexOfSelectedOffice = -1;
+		vm.indexOfSelectedDepartment = -1;
+		vm.selectedDepartmentName = "";
+		vm.selectedDepartment = "";
+		vm.selectedOfficeName = "";
+		vm.selectedOffice = "";
+		vm.showODInformation = false;
+		vm.showEditOfficeForm = false;
+		vm.showEditDepartmentForm = false;
+		vm.showSaveOfficeForm = false;
+		vm.showSaveDepartmentForm = false;
 
-		$scope.init = function() {
-			AdditionService.GetDepartments().then(
+		vm.officeName = "";
+		vm.officeAddress = "";
+		vm.officeDateOfFoundtation = "";
+		vm.officeDateOfFoundtationForEdit = "";
+		vm.officeWorkerNumber = 0;
+
+		vm.departmentName = "";
+		vm.departmentDateOfFoundtation = "";
+		vm.departmentDateOfFoundtationForEdit = "";
+		vm.departmentWorkerNumber = 0;
+
+		vm.officeNameForCreation = "";
+		vm.officeAddressForCreation = "";
+		vm.officeDateOfFoundtationForCreation = "";
+
+		vm.departmentNameForCreation = "";
+		vm.departmentDateOfFoundtationForCreation = "";
+		vm.selectedOfficeIdForCreation = -1;
+
+		vm.roles = [
+			{id: 2, name: 'WORKER_ROLE'}, 
+			{id: 3, name: 'SUPERIOR_ROLE'}
+		];
+		vm.loginName = "";
+		vm.selectedRoleForCreation = "";
+		vm.firstName = "";
+		vm.lastName = "";
+		vm.selectedGender = "";
+		vm.dateOfBirth = "";
+		vm.nationality = "";
+		vm.position = "";
+		vm.dailyWorkHourTotal = "";
+		vm.emailAddres = "";
+		vm.selectedDepartmentIdForWorkerCreation = 0;
+		vm.selectedSuperiorIdForWorkerCreation = "";
+		//Bindable functions
+		vm.init = init;
+		vm.openEditOffice = openEditOffice;
+		vm.openEditDepartment = openEditDepartment;
+		vm.openSaveOffice = openSaveOffice;
+		vm.openSaveDepartment = openSaveDepartment;
+		vm.openSelect = openSelect;
+		vm.setDepartments = setDepartments;
+		vm.listInformation = listInformation;
+		vm.editOffice = editOffice;
+		vm.editDepartment = editDepartment;
+		vm.createNewOffice = createNewOffice;
+		vm.createNewDepartment = createNewDepartment;
+		vm.createNewUserAndWorker = createNewUserAndWorker;
+
+		// *********************** //
+		// Function implementation //
+		// *********************** //
+		function init() {
+			AdditionService.getDepartments().then(
 				function(result) {
-					$scope.departments = result;
+					vm.departments = result;
 				},
 				function(error) {
 				}
 			);
-			AdditionService.GetOffices().then(
+			AdditionService.getOffices().then(
 				function(result) {
-					$scope.offices = result;
+					vm.offices = result;
 				},
 				function(error) {
 				}
 			);
-			AdditionService.GetSuperiors().then(
+			AdditionService.getSuperiors().then(
 				function(result) {
-					$scope.superiors = result;
+					vm.superiors = result;
 				},
 				function(error) {
 				}
 			);
 		};
-		$scope.openEditOffice = function() {
-			$scope.showSaveOfficeForm = false;
-			$scope.showSaveDepartmentForm = false;
-			$scope.showEditOfficeForm = !$scope.showEditOfficeForm;
-		};
-		$scope.openEditDepartment = function() {
-			$scope.showSaveOfficeForm = false;
-			$scope.showSaveDepartmentForm = false;
-			$scope.showEditDepartmentForm = !$scope.showEditDepartmentForm;
-		};
-		$scope.openSaveOffice = function() {
-			$scope.showEditOfficeForm = false;
-			$scope.showEditDepartmentForm = false;
-			$scope.showSaveOfficeForm = !$scope.showSaveOfficeForm;
-		};
-		$scope.openSaveDepartment = function() {
-			$scope.showEditOfficeForm = false;
-			$scope.showEditDepartmentForm = false;
-			$scope.showSaveDepartmentForm = !$scope.showSaveDepartmentForm;
-		};
-		$scope.openSelect = function() {
-			$scope.officeWorkerNumber = 0;
-			$scope.departmentWorkerNumber = 0;
-			$scope.showODInformation = false;
-			$scope.showEditOfficeForm = false;
-			$scope.showEditDepartmentForm = false;
-			$scope.showSaveOfficeForm = false;
-			$scope.showSaveDepartmentForm = false;
-		};
-		$scope.setDepartments = function() {
-			if (!(typeof $scope.departments) || ($scope.departments.length !== 0)) {
-				$scope.departmentsForOffice = [];
-				$scope.selectedDepartmentName = "";
 
-				for (var i = 0, len = $scope.offices.length; i < len; i++) {
-					if($scope.offices[i].name === $scope.selectedOfficeName) {
-						$scope.selectedOffice = $scope.offices[i];
-						$scope.indexOfSelectedOffice = i;
+		function openEditOffice() {
+			vm.showSaveOfficeForm = false;
+			vm.showSaveDepartmentForm = false;
+			vm.showEditOfficeForm = !vm.showEditOfficeForm;
+		};
+
+		function openEditDepartment() {
+			vm.showSaveOfficeForm = false;
+			vm.showSaveDepartmentForm = false;
+			vm.showEditDepartmentForm = !vm.showEditDepartmentForm;
+		};
+
+		function openSaveOffice() {
+			vm.showEditOfficeForm = false;
+			vm.showEditDepartmentForm = false;
+			vm.showSaveOfficeForm = !vm.showSaveOfficeForm;
+		};
+
+		function openSaveDepartment() {
+			vm.showEditOfficeForm = false;
+			vm.showEditDepartmentForm = false;
+			vm.showSaveDepartmentForm = !vm.showSaveDepartmentForm;
+		};
+
+		function openSelect() {
+			vm.officeWorkerNumber = 0;
+			vm.departmentWorkerNumber = 0;
+			vm.showODInformation = false;
+			vm.showEditOfficeForm = false;
+			vm.showEditDepartmentForm = false;
+			vm.showSaveOfficeForm = false;
+			vm.showSaveDepartmentForm = false;
+		};
+
+		function setDepartments() {
+			if (!(typeof vm.departments) || (vm.departments.length !== 0)) {
+				vm.departmentsForOffice = [];
+				vm.selectedDepartmentName = "";
+
+				for (var i = 0, len = vm.offices.length; i < len; i++) {
+					if(vm.offices[i].name === vm.selectedOfficeName) {
+						vm.selectedOffice = vm.offices[i];
+						vm.indexOfSelectedOffice = i;
 						break;
 					}
 				}
-				if(!(typeof $scope.selectedOffice) || ($scope.selectedOffice !== "")) {
-					for (var i = 0, len = $scope.departments.length; i < len; i++) {
-						if ($scope.departments[i].officeId === $scope.selectedOffice.id) {
-							$scope.officeWorkerNumber += $scope.departments[i].workerNumber;
-							$scope.departmentsForOffice.push($scope.departments[i]);
+				if(!(typeof vm.selectedOffice) || (vm.selectedOffice !== "")) {
+					for (var i = 0, len = vm.departments.length; i < len; i++) {
+						if (vm.departments[i].officeId === vm.selectedOffice.id) {
+							vm.officeWorkerNumber += vm.departments[i].workerNumber;
+							vm.departmentsForOffice.push(vm.departments[i]);
 						}
 					}
 				}
 			}
 		};
 
-		$scope.officeName = "";
-		$scope.officeAddress = "";
-		$scope.officeDateOfFoundtation = "";
-		$scope.officeDateOfFoundtationForEdit = "";
-		$scope.officeWorkerNumber = 0;
-		$scope.departmentName = "";
-		$scope.departmentDateOfFoundtation = "";
-		$scope.departmentDateOfFoundtationForEdit = "";
-		$scope.departmentWorkerNumber = 0;
-		
-		$scope.listInformation = function() {
-			for (var i = 0, len = $scope.departments.length; i < len; i++) {
-				if($scope.departments[i].name === $scope.selectedDepartmentName) {
-					$scope.selectedDepartment = $scope.departments[i];
-					$scope.showODInformation = true;
-					$scope.indexOfSelectedDepartment = i;
+		function listInformation() {
+			for (var i = 0, len = vm.departments.length; i < len; i++) {
+				if(vm.departments[i].name === vm.selectedDepartmentName) {
+					vm.selectedDepartment = vm.departments[i];
+					vm.showODInformation = true;
+					vm.indexOfSelectedDepartment = i;
 					break;
 				}
 			}
-			$scope.officeName = $scope.selectedOfficeName;
-			$scope.officeAddress = $scope.selectedOffice.address;
-			$scope.officeDateOfFoundtation = $scope.selectedOffice.dateOfFoundtation;
-			$scope.officeDateOfFoundtationForEdit = moment($scope.selectedOffice.dateOfFoundtation, 'YYYY.MM.DD', false).toDate();
+			vm.officeName = vm.selectedOfficeName;
+			vm.officeAddress = vm.selectedOffice.address;
+			vm.officeDateOfFoundtation = vm.selectedOffice.dateOfFoundtation;
+			vm.officeDateOfFoundtationForEdit = moment(vm.selectedOffice.dateOfFoundtation, 'YYYY.MM.DD', false).toDate();
 
-			$scope.departmentName = $scope.selectedDepartmentName;
-			$scope.departmentDateOfFoundtation = $scope.selectedDepartment.dateOfFoundtation;
-			$scope.departmentDateOfFoundtationForEdit = moment($scope.selectedDepartment.dateOfFoundtation, 'YYYY.MM.DD', false).toDate();
-			$scope.departmentWorkerNumber = $scope.selectedDepartment.workerNumber;
+			vm.departmentName = vm.selectedDepartmentName;
+			vm.departmentDateOfFoundtation = vm.selectedDepartment.dateOfFoundtation;
+			vm.departmentDateOfFoundtationForEdit = moment(vm.selectedDepartment.dateOfFoundtation, 'YYYY.MM.DD', false).toDate();
+			vm.departmentWorkerNumber = vm.selectedDepartment.workerNumber;
 		};
-		$scope.editOffice  = function() {
-			var newDate = moment($scope.officeDateOfFoundtationForEdit).format('YYYY.MM.DD');
-			AdditionService.EditOffice($scope.selectedOffice.id, $scope.officeName, $scope.officeAddress, newDate).then(
+
+		function editOffice() {
+			var newDate = moment(vm.officeDateOfFoundtationForEdit).format('YYYY.MM.DD');
+			AdditionService.editOffice(vm.selectedOffice.id, vm.officeName, vm.officeAddress, newDate).then(
 				function(result) {
-					$scope.offices[$scope.indexOfSelectedOffice].name = $scope.officeName;
-					$scope.offices[$scope.indexOfSelectedOffice].address = $scope.officeAddress;
-					$scope.offices[$scope.indexOfSelectedOffice].dateOfFoundtation = newDate;
-					$scope.officeDateOfFoundtation = newDate;
-				},
-				function(error) {
-				}
-			);
-		};
-		$scope.editDepartment = function() {
-			var newDate = moment($scope.departmentDateOfFoundtationForEdit).format('YYYY.MM.DD');
-			AdditionService.EditDepartment($scope.selectedDepartment.id, $scope.departmentName, newDate, $scope.selectedDepartment.officeId).then(
-				function(result) {
-					$scope.departments[$scope.indexOfSelectedDepartment].name = $scope.departmentName;
-					$scope.departments[$scope.indexOfSelectedDepartment].dateOfFoundtation = newDate;
-					$scope.departmentDateOfFoundtation = newDate;
+					vm.offices[vm.indexOfSelectedOffice].name = vm.officeName;
+					vm.offices[vm.indexOfSelectedOffice].address = vm.officeAddress;
+					vm.offices[vm.indexOfSelectedOffice].dateOfFoundtation = newDate;
+					vm.officeDateOfFoundtation = newDate;
 				},
 				function(error) {
 				}
 			);
 		};
 
-		$scope.officeNameForCreation = "";
-		$scope.officeAddressForCreation = "";
-		$scope.officeDateOfFoundtationForCreation = "";
-		$scope.departmentNameForCreation = "";
-		$scope.departmentDateOfFoundtationForCreation = "";
-		$scope.selectedOfficeIdForCreation = -1;
-
-		$scope.createNewOffice = function() {
-			var newDate = moment($scope.officeDateOfFoundtationForCreation).format('YYYY.MM.DD');
-			AdditionService.SaveOffice($scope.officeNameForCreation, $scope.officeAddressForCreation, newDate).then(
+		function editDepartment() {
+			var newDate = moment(vm.departmentDateOfFoundtationForEdit).format('YYYY.MM.DD');
+			AdditionService.editDepartment(vm.selectedDepartment.id, vm.departmentName, newDate, vm.selectedDepartment.officeId).then(
 				function(result) {
-					$scope.offices.push({
+					vm.departments[vm.indexOfSelectedDepartment].name = vm.departmentName;
+					vm.departments[vm.indexOfSelectedDepartment].dateOfFoundtation = newDate;
+					vm.departmentDateOfFoundtation = newDate;
+				},
+				function(error) {
+				}
+			);
+		};
+
+		function createNewOffice() {
+			var newDate = moment(vm.officeDateOfFoundtationForCreation).format('YYYY.MM.DD');
+			AdditionService.saveOffice(vm.officeNameForCreation, vm.officeAddressForCreation, newDate).then(
+				function(result) {
+					vm.offices.push({
 						id: result.newId,
-						name: $scope.officeNameForCreation,
-						address: $scope.officeAddressForCreation,
+						name: vm.officeNameForCreation,
+						address: vm.officeAddressForCreation,
 						dateOfFoundation: newDate
 					});
 				},
@@ -175,15 +224,16 @@ angular.module('Addition')
 				}
 			);
 		};
-		$scope.createNewDepartment = function() {
-			var newDate = moment($scope.departmentDateOfFoundtationForCreation).format('YYYY.MM.DD');
-			AdditionService.SaveDepartment($scope.departmentNameForCreation, newDate, $scope.selectedOfficeIdForCreation).then(
+
+		function createNewDepartment() {
+			var newDate = moment(vm.departmentDateOfFoundtationForCreation).format('YYYY.MM.DD');
+			AdditionService.saveDepartment(vm.departmentNameForCreation, newDate, vm.selectedOfficeIdForCreation).then(
 				function(result) {
-					$scope.departments.push({
+					vm.departments.push({
 						id: result.newId,
-						name: $scope.departmentNameForCreation,
+						name: vm.departmentNameForCreation,
 						dateOfFoundation: newDate,
-						officeId: $scope.selectedOfficeIdForCreation
+						officeId: vm.selectedOfficeIdForCreation
 					});
 				},
 				function(error) {
@@ -191,37 +241,21 @@ angular.module('Addition')
 			);
 		};
 
-		$scope.roles = [
-			{id: 2, name: 'WORKER_ROLE'}, 
-			{id: 3, name: 'SUPERIOR_ROLE'}
-		];
-		$scope.loginName = "";
-		$scope.selectedRoleForCreation = "";
-		$scope.firstName = "";
-		$scope.lastName = "";
-		$scope.selectedGender = "";
-		$scope.dateOfBirth = "";
-		$scope.nationality = "";
-		$scope.position = "";
-		$scope.dailyWorkHourTotal = "";
-		$scope.emailAddres = "";
-		$scope.selectedDepartmentIdForWorkerCreation = 0;
-		$scope.selectedSuperiorIdForWorkerCreation = "";
-
-		$scope.createNewUserAndWorker = function() {
+		function createNewUserAndWorker() {
 			var userId = 0;
-			AdditionService.SaveUser($scope.loginName, '', $scopeselectedRoleForCreation, $scope.emailAddres).then(
+			AdditionService.saveUser(vm.loginName, '', vmselectedRoleForCreation, vm.emailAddres).then(
 				function(result) {
 					userId = result;
 				},
 				function(error) {
 				}
 			);
-			AdditionService.SaveWorker($scope.firstName, $scope.lastName, $scope.selectedGender, $scope.dateOfBirth, $scope.nationality, $scope.position, $scope.dailyWorkHourTotal, $scope.emailAddres, $scope.selectedSuperiorIdForWorkerCreation, $scope.selectedDepartmentIdForWorkerCreation, userId).then(
+			AdditionService.saveWorker(vm.firstName, vm.lastName, vm.selectedGender, vm.dateOfBirth, vm.nationality, vm.position, vm.dailyWorkHourTotal, vm.emailAddres, vm.selectedSuperiorIdForWorkerCreation, vm.selectedDepartmentIdForWorkerCreation, userId).then(
 				function(result) {
 				},
 				function(error) {
 				}
 			);
 		};
-    }]);
+    }
+})();
--- worktime-core/src/main/webapp/modules/addition/addition.service.js
+++ worktime-core/src/main/webapp/modules/addition/addition.service.js
@@ -1,213 +1,233 @@
-'use strict';
+(function() {
+	'use strict';
 
-angular.module("Addition")
-.factory('AdditionService', ['$http', '$rootScope', '$q', function AdditionServiceFactory($http, $rootScope, $q) {
-	var service = {};
-	service.GetOffices = function() {
-		var deferred = $q.defer();
-		return $http({
-			method : "GET",
-			url : '/api/addition/v1/office',
-			headers : {
-				'Content-Type': 'application/json'
-			}
-		}).then(function successCallback(response) {
+	angular
+		.module("Addition")
+		.factory('AdditionService', AdditionService);
 
-				deferred.resolve(response.data);
-				return deferred.promise;
+	AdditionService.$inject = ['$http', '$rootScope', '$q'];
 
-			}, function errorCallback(response) {
+	function AdditionService($http, $rootScope, $q) {
 
-				deferred.reject(response);
-				return deferred.promise;
-			});
+		var service = {
+			getOffices 	   : getOffices,
+			getDepartments : getDepartments,
+			editOffice 	   : editOffice,
+			editDepartment : editDepartment,
+			saveOffice 	   : saveOffice,
+			saveDepartment : saveDepartment,
+			saveUser 	   : saveUser,
+			saveWorker 	   : saveWorker,
+			getSuperiors   : getSuperiors
+		};
+
+		return service;
+
+		function getOffices() {
+			var deferred = $q.defer();
+			return $http({
+				method : "GET",
+				url : '/api/addition/v1/office',
+				headers : {
+					'Content-Type': 'application/json'
+				}
+			}).then(function successCallback(response) {
+
+					deferred.resolve(response.data);
+					return deferred.promise;
+
+				}, function errorCallback(response) {
+
+					deferred.reject(response);
+					return deferred.promise;
+				});
+		}
+		function getDepartments() {
+			var deferred = $q.defer();
+			return $http({
+				method : "GET",
+				url : '/api/addition/v1/department',
+				headers : {
+					'Content-Type': 'application/json'
+				}
+			}).then(function successCallback(response) {
+
+					deferred.resolve(response.data);
+					return deferred.promise;
+
+				}, function errorCallback(response) {
+
+					deferred.reject(response);
+					return deferred.promise;
+				});
+		}
+		function editOffice(officeId, name, address, dateOfFoundtation) {
+			var deferred = $q.defer();
+			return $http({
+				method : "PUT",
+				url : '/api/addition/v1/office/' + officeId,
+				headers : {
+					'Content-Type': 'application/json'
+				},
+				data: {
+					'name': name,
+					'address': address,
+					'dateOfFoundtation': dateOfFoundtation
+				}
+			}).then(function successCallback(response) {
+
+					deferred.resolve(response.data);
+					return deferred.promise;
+
+				}, function errorCallback(response) {
+
+					deferred.reject(response);
+					return deferred.promise;
+				});
+		}
+		function editDepartment(departmentId, name, dateOfFoundtation, officeId) {
+			var deferred = $q.defer();
+			return $http({
+				method : "PUT",
+				url : '/api/addition/v1/department/' + departmentId,
+				headers : {
+					'Content-Type': 'application/json'
+				},
+				data: {
+					'name': name,
+					'dateOfFoundtation': dateOfFoundtation,
+					'officeId': officeId
+				}
+			}).then(function successCallback(response) {
+
+					deferred.resolve(response.data);
+					return deferred.promise;
+
+				}, function errorCallback(response) {
+
+					deferred.reject(response);
+					return deferred.promise;
+				});
+		}
+		function saveOffice(name, address, dateOfFoundtation) {
+			var deferred = $q.defer();
+			return $http({
+				method : "PUT",
+				url : '/api/addition/v1/office' ,
+				headers : {
+					'Content-Type': 'application/json'
+				},
+				data: {
+					'name': name,
+					'address': address,
+					'dateOfFoundtation': dateOfFoundtation
+				}
+			}).then(function successCallback(response) {
+
+					deferred.resolve(response.data);
+					return deferred.promise;
+
+				}, function errorCallback(response) {
+
+					deferred.reject(response);
+					return deferred.promise;
+				});
+		}
+		function saveDepartment(name, dateOfFoundtation, officeId) {
+			var deferred = $q.defer();
+			return $http({
+				method : "PUT",
+				url : '/api/addition/v1/department',
+				headers : {
+					'Content-Type': 'application/json'
+				},
+				data: {
+					'name': name,
+					'dateOfFoundtation': dateOfFoundtation,
+					'officeId': officeId
+				}
+			}).then(function successCallback(response) {
+
+					deferred.resolve(response.data);
+					return deferred.promise;
+
+				}, function errorCallback(response) {
+
+					deferred.reject(response);
+					return deferred.promise;
+				});
+		}
+		function saveUser(name, password, role) {
+			var deferred = $q.defer();
+			return $http({
+				method : "PUT",
+				url : '/api/addition/v1/user' ,
+				headers : {
+					'Content-Type': 'application/json'
+				},
+				data: {
+					'name': name,
+					'password': password,
+					'role': role
+				}
+			}).then(function successCallback(response) {
+
+					deferred.resolve(response.data);
+					return deferred.promise;
+
+				}, function errorCallback(response) {
+
+					deferred.reject(response);
+					return deferred.promise;
+				});
+		}
+		function saveWorker(firstName, lastName, gender, dateOfBirth, nationality, position, dailyWorkHourTotal, emailAddres) {
+			var deferred = $q.defer();
+			return $http({
+				method : "PUT",
+				url : '/api/addition/v1/worker',
+				headers : {
+					'Content-Type': 'application/json'
+				},
+				data: {
+					'firstName': firstName,
+					'lastName': lastName,
+					'gender': gender,
+					'dateOfBirth': dateOfBirth,
+					'nationality': nationality,
+					'position': position,
+					'dailyWorkHourTotal': dailyWorkHourTotal,
+					'emailAddres': emailAddres
+				}
+			}).then(function successCallback(response) {
+
+					deferred.resolve(response.data);
+					return deferred.promise;
+
+				}, function errorCallback(response) {
+
+					deferred.reject(response);
+					return deferred.promise;
+				});
+		}
+		function getSuperiors() {
+			var deferred = $q.defer();
+			return $http({
+				method : "GET",
+				url : '/api/addition/v1/superior',
+				headers : {
+					'Content-Type': 'application/json'
+				}
+			}).then(function successCallback(response) {
+
+					deferred.resolve(response.data);
+					return deferred.promise;
+
+				}, function errorCallback(response) {
+
+					deferred.reject(response);
+					return deferred.promise;
+				});
+		}
 	}
-	service.GetDepartments = function() {
-		var deferred = $q.defer();
-		return $http({
-			method : "GET",
-			url : '/api/addition/v1/department',
-			headers : {
-				'Content-Type': 'application/json'
-			}
-		}).then(function successCallback(response) {
-
-				deferred.resolve(response.data);
-				return deferred.promise;
-
-			}, function errorCallback(response) {
-
-				deferred.reject(response);
-				return deferred.promise;
-			});
-	}
-	service.EditOffice = function(officeId, name, address, dateOfFoundtation) {
-		var deferred = $q.defer();
-		return $http({
-			method : "PUT",
-			url : '/api/addition/v1/office/' + officeId,
-			headers : {
-				'Content-Type': 'application/json'
-			},
-			data: {
-				'name': name,
-				'address': address,
-				'dateOfFoundtation': dateOfFoundtation
-			}
-		}).then(function successCallback(response) {
-
-				deferred.resolve(response.data);
-				return deferred.promise;
-
-			}, function errorCallback(response) {
-
-				deferred.reject(response);
-				return deferred.promise;
-			});
-	}
-	service.EditDepartment = function(departmentId, name, dateOfFoundtation, officeId) {
-		var deferred = $q.defer();
-		return $http({
-			method : "PUT",
-			url : '/api/addition/v1/department/' + departmentId,
-			headers : {
-				'Content-Type': 'application/json'
-			},
-			data: {
-				'name': name,
-				'dateOfFoundtation': dateOfFoundtation,
-				'officeId': officeId
-			}
-		}).then(function successCallback(response) {
-
-				deferred.resolve(response.data);
-				return deferred.promise;
-
-			}, function errorCallback(response) {
-
-				deferred.reject(response);
-				return deferred.promise;
-			});
-	}
-	service.SaveOffice = function(name, address, dateOfFoundtation) {
-		var deferred = $q.defer();
-		return $http({
-			method : "PUT",
-			url : '/api/addition/v1/office' ,
-			headers : {
-				'Content-Type': 'application/json'
-			},
-			data: {
-				'name': name,
-				'address': address,
-				'dateOfFoundtation': dateOfFoundtation
-			}
-		}).then(function successCallback(response) {
-
-				deferred.resolve(response.data);
-				return deferred.promise;
-
-			}, function errorCallback(response) {
-
-				deferred.reject(response);
-				return deferred.promise;
-			});
-	}
-	service.SaveDepartment = function(name, dateOfFoundtation, officeId) {
-		var deferred = $q.defer();
-		return $http({
-			method : "PUT",
-			url : '/api/addition/v1/department',
-			headers : {
-				'Content-Type': 'application/json'
-			},
-			data: {
-				'name': name,
-				'dateOfFoundtation': dateOfFoundtation,
-				'officeId': officeId
-			}
-		}).then(function successCallback(response) {
-
-				deferred.resolve(response.data);
-				return deferred.promise;
-
-			}, function errorCallback(response) {
-
-				deferred.reject(response);
-				return deferred.promise;
-			});
-	}
-	service.SaveUser = function(name, password, role) {
-		var deferred = $q.defer();
-		return $http({
-			method : "PUT",
-			url : '/api/addition/v1/user' ,
-			headers : {
-				'Content-Type': 'application/json'
-			},
-			data: {
-				'name': name,
-				'password': password,
-				'role': role
-			}
-		}).then(function successCallback(response) {
-
-				deferred.resolve(response.data);
-				return deferred.promise;
-
-			}, function errorCallback(response) {
-
-				deferred.reject(response);
-				return deferred.promise;
-			});
-	}
-	service.SaveWorker = function(firstName, lastName, gender, dateOfBirth, nationality, position, dailyWorkHourTotal, emailAddres) {
-		var deferred = $q.defer();
-		return $http({
-			method : "PUT",
-			url : '/api/addition/v1/worker',
-			headers : {
-				'Content-Type': 'application/json'
-			},
-			data: {
-				'firstName': firstName,
-				'lastName': lastName,
-				'gender': gender,
-				'dateOfBirth': dateOfBirth,
-				'nationality': nationality,
-				'position': position,
-				'dailyWorkHourTotal': dailyWorkHourTotal,
-				'emailAddres': emailAddres
-			}
-		}).then(function successCallback(response) {
-
-				deferred.resolve(response.data);
-				return deferred.promise;
-
-			}, function errorCallback(response) {
-
-				deferred.reject(response);
-				return deferred.promise;
-			});
-	}
-	service.GetSuperiors = function() {
-		var deferred = $q.defer();
-		return $http({
-			method : "GET",
-			url : '/api/addition/v1/superior',
-			headers : {
-				'Content-Type': 'application/json'
-			}
-		}).then(function successCallback(response) {
-
-				deferred.resolve(response.data);
-				return deferred.promise;
-
-			}, function errorCallback(response) {
-
-				deferred.reject(response);
-				return deferred.promise;
-			});
-	}
-	return service;
-}])
+})();
