59efad5f5dd45428c2b21a061e577ef91cc4b363
IstvanBanszki <banszki.istvan@outlook.com>
2016-10-01 14:52:37 +0200

    AngularJS refactor - use component for update password section

---
+++ worktime-core/src/main/webapp/modules/profile/update-password/update-password.component.js
@@ -0,0 +1,72 @@
+(function() {
+	'use strict';
+
+	const templateLoc = ['modules',
+						 'profile',
+						 'update-password',
+						 'update-password.html'].join('/');
+
+	angular
+		.module('Profile')
+		.component('updatePassword', Component());
+
+	function Component() {
+		return {
+			templateUrl: templateLoc,
+			controller: Controller,
+			controllerAs: 'vm'
+		}
+	}
+
+	Controller.$inject = ['$rootScope', 'ProfileService', 'StatusLogService'];
+
+	function Controller($rootScope, ProfileService, StatusLogService) {
+
+		var vm = this;
+		//Bindable variables
+		vm.oldPassword = "";
+		vm.newPassword = "";
+		vm.newPasswordSecond = "";
+		vm.showDuplicateErrorMessage = false;
+		vm.showErrorMessage = false;
+		//Bindable functions
+		vm.changePassword = changePassword;
+		vm.clearPasswords = clearPasswords;
+
+		// *********************** //
+		// Function implementation //
+		// *********************** //
+		function changePassword() {
+			if (vm.newPassword == vm.newPasswordSecond) {
+				if (vm.newPassword != vm.oldPassword) {
+					ProfileService.changePassword($rootScope.userData.loginName, vm.oldPassword, vm.newPassword).then(
+						function(result) {
+							StatusLogService.showStatusLog(result, 'Password change');
+							if (result === 1) {
+								vm.showDuplicateErrorMessage = false;
+								vm.showErrorMessage = false;	
+							} else {
+								vm.showErrorMessage = true;	
+							}
+						},
+						function(error) {
+							vm.showErrorMessage = true;
+						}
+					)
+				}
+				vm.clearPasswords();
+			} else {
+				vm.showDuplicateErrorMessage = true;
+				vm.clearPasswords();
+			}
+		}
+
+		function clearPasswords() {
+			vm.oldPassword = "";
+			vm.newPassword = "";
+			vm.newPasswordSecond = "";
+		}
+
+	}
+
+})();
