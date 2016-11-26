32c229b3edc2dc0676a52698ab3f02821947ded8
IstvanBanszki <banszki.istvan@outlook.com>
2016-11-26 18:06:30 +0100

    Fix the miscalculations of worklog hours while calculated based on entry logs

--- worktime-sql/src/main/resources/stored_procedure/save_worklog_by_entry.sql
+++ worktime-sql/src/main/resources/stored_procedure/save_worklog_by_entry.sql
@@ -5,25 +5,26 @@ BEGIN
 
 	DECLARE begin_time 		TIMESTAMP;
 	DECLARE end_time 		TIMESTAMP;
-	DECLARE date_of_work 	DATE;
-	DECLARE sub_result_hour	INT;
+	DECLARE sub_result_hour	TINYINT;
+	DECLARE sub_result_min	TINYINT;
 	DECLARE timestamp_num	INT;
 	DECLARE worklog_id		INT;
+	DECLARE worklog_min		TINYINT;
 
-	SELECT Max(`log_timestamp`)
+	SELECT MAX(`log_timestamp`)
 	  INTO begin_time
 	  FROM `entry`
 	 WHERE `worker_id` = worker_id
 	   AND `in_out` = 1;
 
-	SELECT Max(`log_timestamp`)
+	SELECT MAX(`log_timestamp`)
 	  INTO end_time
 	  FROM `entry`
 	 WHERE `worker_id` = worker_id
 	   AND `in_out` = 0;
 
-	SELECT TIMESTAMPDIFF(HOUR, begin_time, end_time)
-	  INTO sub_result_hour;
+	SET sub_result_hour = TIMESTAMPDIFF(HOUR, begin_time, end_time);
+	SET sub_result_min = TIMESTAMPDIFF(MINUTE, begin_time, end_time) - (sub_result_hour * 60);
 
 	SELECT COUNT(`log_timestamp`)
       INTO timestamp_num
@@ -33,11 +34,17 @@ BEGIN
 
 	IF (timestamp_num > 1) THEN
 		BEGIN
-			SELECT `id`
-			  INTO worklog_id
+			SELECT `id`, `work_min`
+			  INTO worklog_id, worklog_min
               FROM `worklog`
              WHERE DATE(`begin_date`) = DATE_FORMAT(begin_time, '%Y-%m-%d');
-        
+
+			SET sub_result_min = worklog_min + sub_result_min;
+			IF (sub_result_min >= 60) THEN
+				SET sub_result_min = sub_result_min - 60;
+				SET sub_result_hour = sub_result_hour + 1;
+			END IF;
+
 			UPDATE `worktime`.`worklog`
 			SET `work_hour` = `work_hour` + sub_result_hour,
 				`date_of_modification` = NOW()
@@ -47,8 +54,8 @@ BEGIN
 		END;
     ELSE
 		BEGIN
-			INSERT INTO `worktime`.`worklog` (`begin_date`, `work_hour`, `worker_id`, `note`)
-			VALUES (DATE_FORMAT(begin_time, '%Y-%m-%d'), sub_result_hour, worker_id, '');
+			INSERT INTO `worktime`.`worklog` (`begin_date`, `work_hour`, `work_min`, `worker_id`, `note`)
+			VALUES (DATE_FORMAT(begin_time, '%Y-%m-%d'), sub_result_hour, sub_result_min, worker_id, '');
 
 			SELECT ROW_COUNT() AS status, LAST_INSERT_ID() AS new_id;
 		END;
